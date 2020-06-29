c3d0b870dc5fb11bdbbe28220c760b3c831f543c
Marcus Chiu <marcuschiu9@gmail.com>
2020-06-29 17:52:42 -0500

    added bin

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
+# server is using nvm, so source .bashrc into to activate it
+# this allows us to use `node`
+source /home/marcuschiu/.bashrc
+
 cd /home/marcuschiu/meet-marcuschiu-com
 node index.js
